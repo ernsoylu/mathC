79fbfc9b09c319ba63d831af15c80b7a7fb950ae
Eren Soylu <ernsoylu@gmail.com>
2024-11-04 14:16:44 +0000

    First edition of the card example.

---
+++ headfirstC/ch1/ch1_1.c
@@ -0,0 +1,12 @@
+/* 
+ * This line for the name of the program and purpose of the program
+ * This line for the licencing options and owner of the program
+ * This line might for time date and change information etc.
+ */
+
+#include <stdio.h>
+
+int main(){
+  puts("What will we write on the screen?");
+  return 0;
+}
