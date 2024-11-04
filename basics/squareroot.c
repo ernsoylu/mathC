21355e02f8709fd2882967ecd20ee1c1781c0577
Eren Soylu <ernsoylu@gmail.com>
2024-11-04 13:14:46 +0000

    Last changes from Macbook

--- basics/squareroot.c
+++ basics/squareroot.c
@@ -1,6 +1,7 @@
-/* Implementation Square-Root operation using Taylor Approximation
- * Reference: https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
- * A program by: Eren SOYLU
+/*
+Implementation Square-Root operation using Taylor Approximation
+Reference: https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+A program by: Eren SOYLU 
 */
 
 
@@ -11,19 +12,22 @@
 #define EPS 0.00001
 
 float sqrtTA(float x);
+float Q_rsqrt(float x);
+float sqrtNT(float x);
 
-int main(int argc, char *argv[])
-{
-  printf("Calculation of Square-Root\n");
-  printf("The Number    Library SR      Taylor SR       Error\n");
-  for(int i = 0;i < 1001; i++){
-    float ansL = sqrt((float)i*EPS);
-    float ansTA = sqrtTA((float)i*EPS);
-    printf("%.5f          %.5f            %.5f            %.10f\n", i*EPS, ansL, ansTA, ansL-ansTA);
-  }
+int main(int argc, char *argv[]) {
+  printf("%.5f\n", Q_rsqrt(144.0));
+  // printf("Calculation of Square-Root\n");
+  // printf("The Number    Library SR      Quake II SR       Error\n");
+  // for(int i = 0;i < 1001; i++){
+    // float ansL = sqrt((float)i);
+    // float ansTA = sqrtTA((float)i);
+    // printf("%.5f  %.5f  %.5f  %.10f\n", (float)i, sqrt(i), Q_rsqrt((float)i), sqrt(i) - Q_rsqrt((float)i));
+  // }
   return EXIT_SUCCESS;
 }
 
+//Taylor Expansion for finding Square-Root
 float sqrtTA(float x){
   if(x>0){
     float k = 0.00000;
@@ -36,4 +40,20 @@ float sqrtTA(float x){
   } else {
     return 0;
   }
+}
+
+float sqrtNT(float x){
+  float initialGuess = EPS;
+
+  return 0;
+}
+
+//Greg Shaw's Quake III areano Fast Inverse Square-Root Algorithm
+float Q_rsqrt(float x) {
+  float xhalf = 0.5F*x;
+  int i = *(int*)&x;
+  i = 0x5f3759df - (i>>1);
+  x = *(float*)&i;
+  x = x*(1.5F-xhalf*x*x);
+  return x;
 }
